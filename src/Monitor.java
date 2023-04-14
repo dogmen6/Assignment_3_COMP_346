127a362ec32cc473cbe4b4ddda974c89950a1d40
Fadoua Doghmane <95242795+dogmen6@users.noreply.github.com>
2023-04-14 19:58:57 -0400

    Update Monitor.java

--- src/Monitor.java
+++ src/Monitor.java
@@ -1,11 +1,18 @@
+
 /**
  * Class Monitor
  * To synchronize dining philosophers.
  *
  * @author Serguei A. Mokhov, mokhov@cs.concordia.ca
  */
-public class Monitor {
-	// STUDENT
+public class Monitor
+{
+	/*
+	 * ------------
+	 * Data members
+	 * ------------
+	 */
+
 	int count;
 	boolean[] eating;
 	boolean speaking = false;
@@ -13,29 +20,39 @@ public class Monitor {
 	/**
 	 * Constructor
 	 */
-	public Monitor(int piNumberOfPhilosophers) {
-		// STUDENT
+	public Monitor(int piNumberOfPhilosophers)
+	{
 		count = piNumberOfPhilosophers;
 		eating = new boolean[count];
 	}
+
+	/*
+	 * -------------------------------
+	 * User-defined monitor procedures
+	 * -------------------------------
+	 */
+
 
 	/**
 	 * Grants request (returns) to eat when both chopsticks/forks are available.
 	 * Else forces the philosopher to wait()
 	 */
-	public synchronized void pickUp(final int piTID) {
-		// STUDENT
-		try {
-			// While either of my neighbours are eating, I cannot eat
-			while (eating[(piTID + 1) % count] || eating[(piTID + (count - 1)) % count]) {
+	public synchronized void pickUp(final int piTID)
+	{
+		try{
+			//while either of my neighbours are eating, I cannot eat
+			while (eating[(piTID + 1) % count] || eating[(piTID + (count-1)) % count]){
 				wait();
 			}
-			// When both neighbours are done eating, I start eating
-			eating[piTID] = true;
-		} catch (InterruptedException e) {
-			System.err.println("Monitor.pickUp():");
-			DiningPhilosophers.reportException(e);
-			System.exit(1);
+
+
+		//when both neighbours are done eating, I can start eating
+		eating[piTID-1] = true;
+	}
+		catch(InterruptedException e){
+		System.err.println("Monitor.pickUp():");
+		DiningPhilosophers.reportException(e);
+		System.exit(1);
 		}
 	}
 
@@ -43,9 +60,9 @@ public class Monitor {
 	 * When a given philosopher's done eating, they put the chopstiks/forks down
 	 * and let others know they are available.
 	 */
-	public synchronized void putDown(final int piTID) {
-		// STUDENT
-		eating[piTID] = false;
+	public synchronized void putDown(final int piTID)
+	{
+		eating[piTID-1] = false;
 		notifyAll();
 	}
 
@@ -53,16 +70,17 @@ public class Monitor {
 	 * Only one philopher at a time is allowed to philosophy
 	 * (while she is not eating).
 	 */
-	public synchronized void requestTalk() {
-		// STUDENT
-		try {
-			// I cannot talk as long as someone else is speaking
-			while (speaking) {
+	public synchronized void requestTalk()
+	{
+		try{
+			//I cannot talk as long as someone else speaks
+			while(speaking){
 				wait();
 			}
-			// When no one else is talking, I can start talking
+			//when no one else talks, I can talk
 			speaking = true;
-		} catch (InterruptedException e) {
+		}
+		catch (InterruptedException e) {
 			System.err.println("Monitor.requestTalk():");
 			DiningPhilosophers.reportException(e);
 			System.exit(1);
@@ -73,9 +91,11 @@ public class Monitor {
 	 * When one philosopher is done talking stuff, others
 	 * can feel free to start talking.
 	 */
-	public synchronized void endTalk() {
-		// STUDENT
+	public synchronized void endTalk()
+	{
 		speaking = false;
 		notifyAll();
 	}
 }
+
+// EOF
